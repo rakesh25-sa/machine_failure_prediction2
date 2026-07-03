f0156d48f84d1ba2975e1c148d13d5b7476ff85e
rakesh25-sa <rakeshambudkar@gmail.com>
2026-07-03 16:19:47 +0000

    week_3_mls pipeline - secrets via env

---
+++ week_3_mls/hosting/hosting.py
@@ -0,0 +1,16 @@
+from huggingface_hub import HfApi
+import os
+
+from google.colab import userdata
+
+os.environ["HF_TOKEN"] = userdata.get("HF_TOKEN")    # please use your token
+
+api = HfApi(token=os.getenv("HF_TOKEN"))
+api.upload_folder(
+    folder_path="week_3_mls/deployment",     # the local folder containing your files
+    # replace with your repoid
+    repo_id="rakeshambudkar/Machine-Failure-Prediction",          # the target repo
+
+    repo_type="space",                      # dataset, model, or space
+    path_in_repo="",                          # optional: subfolder path inside the repo
+)
